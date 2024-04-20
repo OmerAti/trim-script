4f77bbb45e833056134bab2e1e386761baade6a0
Ömer ATABER <67821076+OmerAti@users.noreply.github.com>
2024-04-20 17:47:42 +0300

    Update trim.sh

--- trim.sh
+++ trim.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
-#JRodix.Com Internet Hizmetleri Omer ATABER - By OmerAti
+#JRodiX.Com Internet Hizmetleri Omer ATABER - By OmerAti
 trimmed_amount=$(fstrim -v / | awk '{print $2, $3}')
-echo "Trim islemi basariyla tamamlandi $trimmed_amount alan temizlendi ve optimize edildi "
+echo "Trim islemi basariyla tamamlandi. $trimmed_amount alan temizlendi ve optimize edildi."
+
+cat <<EOF > /etc/systemd/system/fstrim.service
+[Unit]
+Description=Discard unused blocks on the root filesystem
+
+[Service]
+Type=oneshot
+ExecStart=/usr/sbin/fstrim -av
+EOF
+
 cat <<EOF > /etc/systemd/system/fstrim.timer
 [Unit]
 Description=Discard unused blocks once a week
@@ -16,5 +26,6 @@ WantedBy=timers.target
 EOF
 
 systemctl daemon-reload
+systemctl enable fstrim.service
 systemctl enable fstrim.timer
 systemctl start fstrim.timer
